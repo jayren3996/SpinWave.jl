866702ac686148dfc51a318dac0564c918e1be58
JayRen <jieren3996@foxmail.com>
2020-07-02 20:00:00 +0800

    Update demo.jl

--- demo.jl
+++ demo.jl
@@ -12,22 +12,18 @@ mat = begin                                    # interaction term.
     m
 end
 
-θ = [0, π, π, 0] # spin direction.
-ϕ = [0, 0, 0, 0] # spin direction.
-t = [0 0; 0.5 0; 0 0.5; 0.5 0.5] # sub-lattice index.
-αβ = [1 1;2 2;3 3] # spin summation indeces.
-η = 0.1 # quasi-particle lifetime.
+θ = [0, π, π, 0]                               # spin direction.
+ϕ = [0, 0, 0, 0]                               # spin direction.
+t = [0 0; 0.5 0; 0 0.5; 0.5 0.5]               # sub-lattice index.
 
-s = spinsystem(θ,ϕ,ind,vec,mat,t,αβ,η) # cunstruct spin system
+s = spinsystem(θ,ϕ,ind,vec,mat,t)              # cunstruct spin system
 #--- spin wave
 ωs = range(0, 5, length=200)
 kp = kpoints([0 0;1 0;1 1;0 0],[100, 100, 141])
 
 corr, spec = spinwave(s, kp, ωs)
-
 #--- Plot
-cmax = 3.0
-@. corr[corr>cmax] = cmax
+@. corr[corr>3.0] = 3.0
 pygui(true)
 figure(dpi=200)
 contourf(1:kp.N,ωs,corr',levels=50,cmap="jet")
